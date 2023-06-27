cca7c085a697e886a6c9c46f06600f056743bf1b
Charanreddy864 <83538418+Charanreddy864@users.noreply.github.com>
2023-06-27 11:27:15 +0530

    Update charan.js

--- charan.js
+++ charan.js
@@ -2,3 +2,11 @@ function scrolldown(className) {
   var div = document.getElementsByClassName(className);
     div[0].scrollIntoView({ behavior: "smooth" });
 }
+function showPopup()
+{
+    document.getElementsByClassName("popup")[0].classList.add("popped");
+}
+function hidePopup()
+{
+    document.getElementsByClassName("popup")[0].classList.remove("popped");
+}

644dc8e1a94a24a691a3920ba02af1adf9459411
Charanreddy864 <83538418+Charanreddy864@users.noreply.github.com>
2023-07-09 21:49:45 +0530

    Update charan.js

--- charan.js
+++ charan.js
@@ -10,3 +10,19 @@ function hidePopup()
 {
     document.getElementsByClassName("popup")[0].classList.remove("popped");
 }
+let expanded = false;
+function toggleMenu() {
+  expanded = !expanded;
+  var icon = document.querySelector(".mobile i");
+  var list=document.querySelector(".navlinks");
+  if (expanded) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+    list.classList.add("active");
+  } else {
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+    list.classList.remove("active");
+  }
+}
+
